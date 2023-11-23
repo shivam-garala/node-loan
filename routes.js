eca089d1328d0c5e83a50ebec2dab7f0839b72e3
Shivam Garala <shivam@phpdots.com>
2023-11-24 00:14:59 +0530

    validations changes

--- routes.js
+++ routes.js
@@ -35,7 +35,37 @@ Router.post("/createUser",  [
     }),
   ], controller().createUser);
 
-Router.post("/addForm",  controller().createForm);
+Router.post("/addForm", [
+
+  body("amount").not().isEmpty().withMessage("Enter amount"),
+  body("disbursal").not().isEmpty().withMessage("Enter disbursal"),
+  body("interest").not().isEmpty().withMessage("Enter interest"),
+  body("repayment").not().isEmpty().withMessage("Enter repayment"),
+  body("service_charge").not().isEmpty().withMessage("Enter service charge"),
+  body("received_amount").not().isEmpty().withMessage("Enter recieved amount"),
+  body("gst").not().isEmpty().withMessage("Enter gst"),
+  body("month").not().isEmpty().withMessage("Enter month"),
+  body("aadhaar_number").not().isEmpty().withMessage("Enter your aadhaar number"),
+  body("pan_number").not().isEmpty().withMessage("Enter your pan number"),
+  body("first_name").not().isEmpty().withMessage("Enter your first name"),
+  body("last_name").not().isEmpty().withMessage("Enter your last name"),
+  body("email").not().isEmpty().withMessage("Enter your email"),
+  body("mobile_number").not().isEmpty().withMessage("Enter your mobile number"),
+  body("birth_date").not().isEmpty().withMessage("Enter your birth date"),
+  body("gender").not().isEmpty().withMessage("Enter your gender"),
+  body("address").not().isEmpty().withMessage("Enter your address"),
+  body("city").not().isEmpty().withMessage("Enter city"),
+  body("state").not().isEmpty().withMessage("Enter state"),
+  body("country").not().isEmpty().withMessage("Enter country"),
+  body("pincode").not().isEmpty().withMessage("Enter pincode"),
+  body("monthly_income").not().isEmpty().withMessage("Enter your monthly income"),
+  body("profession").not().isEmpty().withMessage("Enter your profession"),
+  body("bank_name").not().isEmpty().withMessage("Enter bank name"),
+  body("account_holder_name").not().isEmpty().withMessage("Enter account holder name"),
+  body("account_number").not().isEmpty().withMessage("Enter account number"),
+  body("ifsc_code").not().isEmpty().withMessage("Enter ifsc code"),
+
+],  controller().createForm);
 
 Router.get("/loanAmount/:user_id",  controller().getLoanAmount);
 
